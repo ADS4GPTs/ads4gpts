3cb617d91c6649954caa82db78d18f933580bf6d
knitlydevaccount <119147597+knitlydevaccount@users.noreply.github.com>
2024-11-27 09:53:27 +0200

    Remote graph example

--- libs/python-sdk/ads4gpts-langchain/setup.py
+++ libs/python-sdk/ads4gpts-langchain/setup.py
@@ -2,16 +2,17 @@ import toml
 from setuptools import setup, find_packages
 
 # Load the pyproject.toml file
-with open("pyproject.toml", "r") as pyproject_file:
+with open("./pyproject.toml", "r") as pyproject_file:
     pyproject_data = toml.load(pyproject_file)
 
 # Extract project metadata
 tool_poetry = pyproject_data.get("tool", {}).get("poetry", {})
+print(tool_poetry)
 name = tool_poetry.get("name", "default_name")
 version = tool_poetry.get("version", "0.0.1")
 description = tool_poetry.get("description", "")
 authors = tool_poetry.get("authors", [])
-license = tool_poetry.get("license", "")
+license_name = tool_poetry.get("license", "")
 readme = tool_poetry.get("readme", "")
 homepage = tool_poetry.get("homepage", "")
 repository = tool_poetry.get("repository", "")
@@ -20,11 +21,6 @@ keywords = tool_poetry.get("keywords", [])
 classifiers = tool_poetry.get("classifiers", [])
 dependencies = tool_poetry.get("dependencies", {})
 
-# Process authors
-author_list = [author.split(" <")[0] for author in authors]
-author_email_list = [
-    author.split(" <")[1].rstrip(">") if " <" in author else "" for author in authors
-]
 
 # Process dependencies
 install_requires = [
@@ -33,13 +29,26 @@ install_requires = [
     if pkg != "python"
 ]
 
+# Adjust version specifiers to comply with PEP 440
+install_requires = [
+    dep.replace("^", ">=") if "^" in dep else dep for dep in install_requires
+]
+
+# Process python_requires
+python_version = dependencies.get("python", "")
+if python_version.startswith("^"):
+    base_version = python_version[1:]
+    major, minor = base_version.split(".")
+    python_requires = f">={base_version},<{int(major) + 1}.0"
+else:
+    python_requires = python_version
+
 setup(
     name=name,
     version=version,
     description=description,
-    author=", ".join(author_list),
-    author_email=", ".join(author_email_list),
-    license=license,
+    author=", ".join(authors),
+    license=license_name,
     long_description=open(readme).read() if readme else "",
     long_description_content_type="text/markdown",
     url=homepage,
@@ -51,5 +60,5 @@ setup(
     keywords=keywords,
     packages=find_packages(where="."),
     install_requires=install_requires,
-    python_requires=dependencies.get("python", ""),
+    python_requires=python_requires,
 )
