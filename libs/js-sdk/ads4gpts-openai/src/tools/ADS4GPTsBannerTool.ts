584ac6e50089b37e2e6e19dead3cbad51453ffbc
Anastasios Lianos <loom+github@knitly.app>
2025-01-15 15:08:28 +0200

    refactor: ads4gpts-openai docstrings

--- libs/js-sdk/ads4gpts-openai/src/tools/ADS4GPTsBannerTool.ts
+++ libs/js-sdk/ads4gpts-openai/src/tools/ADS4GPTsBannerTool.ts
@@ -7,7 +7,15 @@ export class ADS4GPTsBannerTool extends ADS4GPTsTool<
     BannerAdsPayload,
     BannerAdData | BannerAdData[]
 > {
+    /**
+     *
+     * @param args - (apiKey, baseUrl) override or environment-based config.
+     * @param args.apiKey - (optional) The ADS4GPTs API key to use for the tool.
+     * You can either pass it as an argument or set it as an environment variable.
+     * @param args.baseUrl - (optional) The base URL for the ADS4GPTs API service. It
+     * defaults to the ADS4GPTS API URL.
+     */
     constructor(args: { apiKey?: string; baseUrl?: string } = {}) {
-        super(args, 'ads4gpts_banner_tool', true);
+        super(args, 'ads4gpts_banner_tool');
     }
 }
